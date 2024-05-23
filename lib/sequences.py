c13087593b2c44f7db394a2e195c28b0279552f7
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-05-23 19:17:06 +0300

    sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_seq = []
+    
+    if length > 0:
+        fib_seq.append(0)  # Add the first Fibonacci number
+    if length > 1:
+        fib_seq.append(1)  # Add the second Fibonacci number
+        
+    for i in range(2, length):
+        next_value = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_value)
+    
+    print(fib_seq)
+
+
+print_fibonacci(10)
